65df8f5d83674de45ea2f77a3386706ae689e964
folder structure added <moinaktarshaikh651@gmail.com>
2025-09-22 08:55:46 +0530

    huggingface token importing

--- backend/src/helper.py
+++ backend/src/helper.py
@@ -1,21 +1,18 @@
+import os
 from langchain_community.document_loaders import DirectoryLoader, PyPDFLoader
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.embeddings import HuggingFaceInferenceAPIEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from typing import List
 from langchain.schema import Document
 
 
-#Extract Data From the PDF File
+# Extract Data From the PDF File
 def load_pdf_file(data):
-    loader= DirectoryLoader(data,
-                            glob="*.pdf",
-                            loader_cls=PyPDFLoader)
-
-    documents=loader.load()
-
+    loader = DirectoryLoader(data,
+                             glob="*.pdf",
+                             loader_cls=PyPDFLoader)
+    documents = loader.load()
     return documents
-
-
 
 
 def filter_to_minimal_docs(docs: List[Document]) -> List[Document]:
@@ -35,16 +32,21 @@ def filter_to_minimal_docs(docs: List[Document]) -> List[Document]:
     return minimal_docs
 
 
-
-#Split the Data into Text Chunks
+# Split the Data into Text Chunks
 def text_split(extracted_data):
-    text_splitter=RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=20)
-    text_chunks=text_splitter.split_documents(extracted_data)
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=20)
+    text_chunks = text_splitter.split_documents(extracted_data)
     return text_chunks
 
 
-
-#Download the Embeddings from HuggingFace 
+# Download the Embeddings from HuggingFace Inference API
 def download_hugging_face_embeddings():
-    embeddings=HuggingFaceEmbeddings(model_name='sentence-transformers/paraphrase-MiniLM-L3-v2')  #this model return 384 dimensions
+    hf_token = os.getenv("HF_API_TOKEN")
+    if not hf_token:
+        raise ValueError("Missing Hugging Face API token (HF_API_TOKEN)")
+
+    embeddings = HuggingFaceInferenceAPIEmbeddings(
+        api_key=hf_token,
+        model_name="sentence-transformers/all-MiniLM-L6-v2"  # 384 dims
+    )
     return embeddings
